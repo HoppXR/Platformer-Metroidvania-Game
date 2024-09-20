08ed35bfc62e46b3db04edea90a4191790684eb4
Richard w <kiwi348750059@gmail.com>
2024-09-19 22:22:46 -0400

    Reapply "Merge branch 'main' into Richard"
    This reverts commit 521756cdb39982b7d70fb84f774f3248c5da6667.

---
+++ Assets/Scripts/Collectable.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class Collectable : MonoBehaviour
+{
+    private GameManager _gameManager;
+    
+    private float _initialY;
+
+    private void Start()
+    {
+        _gameManager = FindFirstObjectByType<GameManager>();
+        _initialY = transform.position.y;
+    }
+
+    private void FixedUpdate()
+    {
+        HandleMovement();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            // add collectable logic
+            _gameManager.IncreaseCount();
+            
+            Destroy(gameObject);
+        }
+    }
+
+    private void HandleMovement()
+    {
+        transform.Rotate(0f,1f,0f);
+        transform.position = new Vector3(transform.position.x, 0.35f * Mathf.Sin(Time.time * 1f) + _initialY, transform.position.z);
+    }
+}
