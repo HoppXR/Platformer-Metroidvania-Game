29a28f035bc3bb771e26da7d825bf257ca6b418f
MaxFreya <144817277+MaxFreya42@users.noreply.github.com>
2025-03-30 07:19:24 -0400

    collision fixes and spork dialogue

--- Assets/Scripts/Player/Interaction/SporkInteraction.cs
+++ Assets/Scripts/Player/Interaction/SporkInteraction.cs
@@ -11,14 +11,25 @@ namespace Player.Interaction
         [SerializeField] private GameObject TextWindow;
         [SerializeField] private TextMeshProUGUI tutorialText;
         
-        string[] dialogue = { "Hey kid, you okay?", "test 2", "Test 3, wow this works! " };
+        string[] dialogue = { 
+            "Hey kid! you okay?",
+            "I saw the whole thing from right here.",
+            "Those punk Flying Tea-Kettles and their no good lead Sir Earl of Grey “repossessed” your entire house?",
+            "Well that just ain’t right! They've been nothin’ but trouble ever since they built their VAULT here in the swamp.",
+            "Say you're a Swamp Wampus right?",
+            "HA HA thought so! The tails give it away.",
+            "I reckon you could get powerful enough to take the fight right to their doorstep if you powered yourself up a tad.",
+            "I’ve heard talk of power gems around the swamp that you Swamp Wampus’ can use to learn secret skills!",
+            "I bet findin’ some of those might prove useful in taking those self righteous Tea-Kettles down a peg or two",
+            "If you wanna get a lay of the land try pausing the game to check the map!.. Whatever that means...",
+        };
         int dialogueTracker;
         [SerializeField] private int dialogueLoopPoint;
         
         public void Interact()
         {
             
-            if (dialogueTracker <= 2)
+            if (dialogueTracker <= 9)
             {
                 tutorialText.text = dialogue[dialogueTracker];
                 TextWindow.SetActive(true);
@@ -28,7 +39,7 @@ namespace Player.Interaction
             else if (dialogueTracker > 2) {
             
                 TextWindow.SetActive(false);
-                dialogueTracker = 0;
+                dialogueTracker = dialogueLoopPoint;
             }
         }
         public string GetInteractText()
